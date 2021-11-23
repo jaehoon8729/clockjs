52b561b3face03f220a92464d3804491e5226c20
jaehoon <jaehoon8729@naver.com>
2021-11-23 11:08:17 +0900

    option final

--- todo.js
+++ todo.js
@@ -4,15 +4,17 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos";
 
-const toDos = [];
+let toDos = [];
 
 function deleteToDo(event){
     const btn = event.target;
     const li = btn.parentNode;
     toDoList.removeChild(li);
     const cleanToDos = toDos.filter(function(toDo){
-        return toDo.id !== li.id;
+        return toDo.id !== parseInt(li.id);
     });
+    toDos = cleanToDos;
+    saveToDos();
 }
 
 function saveToDos(){
@@ -47,9 +49,9 @@ function handleSubmit(event){
 }
 
 function loadToDos(){
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos !== null){
-        const parsedToDos= JSON.parse(loadToDos);
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null){
+        const parsedToDos = JSON.parse(loadedToDos);
         parsedToDos.forEach(function(toDo){
             paintToDo(toDo.text);
         });
@@ -60,5 +62,4 @@ function init(){
     loadToDos();
     toDoForm.addEventListener("submit", handleSubmit);
 }
-
 init();
