5e8a74b9d95b0e8d935c36ffbc352f2c4349ddb8
jaehoon <jaehoon8729@naver.com>
2021-11-24 10:56:05 +0900

    mobileBGI

--- bg.js
+++ bg.js
@@ -1,14 +1,27 @@
 const body = document.querySelector("body");
 
 const IMG_NUMBER=3;
+var is_mobile = isMobile();
 
-function handleImgLoad(){
-
+function isMobile() { 
+    var user = navigator.userAgent; 
+    var is_mobile = false; 
+    if( user.indexOf("iPhone") > -1 
+        || user.indexOf("Android") > -1 
+        || user.indexOf("iPad") > -1 
+        || user.indexOf("iPod") > -1 ){ 
+        is_mobile = true; 
+    } 
+    return is_mobile; 
 }
 
 function paintImage(imgNumber){
     const image = new Image();
-    image.src = `./images/${imgNumber + 1}.jpg`;
+    if( is_mobile ) {
+        image.src = `./mbImages/${imgNumber + 1}.jpg`;
+    }else{
+        image.src = `./mbImages/${imgNumber + 1}.jpg`;
+    }
     image.classList.add("bgImage");
     body.prepend(image);
 }
