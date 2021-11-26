512cbb7c1dcaa794c44f59d2e2ab0bc4d3c6ce40
jaehoon <jaehoon8729@naver.com>
2021-11-26 14:03:24 +0900

    rm

--- isMobile.js
+++ isMobile.js
@@ -12,21 +12,3 @@ function isMobile() {
     return is_mobile; 
 }
 isMobile();
-
-// function resize(){
-//     toDolists.style.fontSize="50px";
-//     toDospans.style.fontSize="50px";
-//     toDoButton.style.padding="15px 15px";
-//     h1.style.fontSize="100px";
-//     h3.style.fontSize="80px";
-//     toDoInput.style.height="60px";
-//     toDoInput.style.fontSize="50px";
-//     toDoInput.style.backgroundPosition="60px 50px";
-// }
-
-// function init(){
-//     if( is_mobile ) {
-//         resize();
-//     }
-// }
-// init();
